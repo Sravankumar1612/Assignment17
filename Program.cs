29feace0106304eb0d148571a5590d404d0aab58
Sravankumar1612 <sravankumartikkavarapu@gmail.com>
2023-07-27 15:40:38 +0530

    intial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(5);
+            circle.PrintInfo();
+
+            Rectangle rectangle = new Rectangle(5, 2);
+            rectangle.PrintInfo();
+
+            Triangle triangle = new Triangle(3, 5, 9);
+            triangle.PrintInfo();
+            Console.ReadKey();
+        }
+    }
+}
